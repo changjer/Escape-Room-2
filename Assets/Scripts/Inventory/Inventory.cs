2661d7d7c17e31822af1a787356d7c20d55d2e6e
Carl S <sinocruc@oregonstate.edu>
2020-04-26 22:49:10 -0700

    Inventory UI added along with scripting for the inventory and for items that can be added to the inventory. Still need to find a way to have the item image appear in the UI.

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject inventory;
+    public GameObject inventoryPanel;
+    public GameObject[] inventorySlot;
+    private static int _inventorySize = 6;
+
+    void Start()
+    {
+        InitializeInventory();
+    }
+
+    public void InitializeInventory()
+    {
+        inventorySlot = new GameObject[_inventorySize];
+        
+        for(int i = 0; i < _inventorySize; i++)
+        {
+            inventorySlot[i] = inventoryPanel.transform.GetChild(i).gameObject;
+            Debug.Log("Initialize inventorySlot[" + i + "]: " + inventorySlot[i]);
+        }
+    }
+    public void AddItem(GameObject item)
+    {
+        bool itemAdded = false;
+        //find open slot in inventory array
+        for(int i = 0; i < inventorySlot.Length; i++)
+        {
+            //need to check that slot does not contain an item
+            if(inventorySlot[i].gameObject.tag != "InventoryItem")
+            {   
+                inventorySlot[i] = item;
+                item.SetActive(false); //removes item from scene
+                //inventorySlot[i]..text = item.GetComponent<Item>().name;
+                Debug.Log(item.name + " was added to inventory");
+                itemAdded = true;
+                break;
+            }
+        }
+        if(itemAdded == false)
+        {
+            Debug.Log("Inventory Full - Item Not Added");
+        }
+    }
+    public void RemoveItem(int index)
+    {
+        inventorySlot[index].gameObject.SetActive(true);
+        inventorySlot[index] = inventoryPanel.transform.GetChild(index).gameObject;
+        Debug.Log("Removing Item");
+    }
+}
---
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItem : MonoBehaviour
+{
+    public Inventory inventory;
+    public new string name;
+    public string description;
+    //public Sprite icon;
+    void Start()
+    {
+        Print();
+    }
+    void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("User Selected Item: " + this.gameObject.GetComponent<InventoryItem>().name);
+            inventory.gameObject.GetComponent<Inventory>().AddItem(this.gameObject); 
+        }   
+    }
+    public void Print()
+    {
+        Debug.Log(this.name + "[" + this.description + "] has been created");
+    }
+}
