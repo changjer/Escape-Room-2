c60535956ed86b563320a0df7265f70fa27d38ab
changjer <35316534+changjer@users.noreply.github.com>
2020-05-25 00:44:21 -0700

    Computer password verificaiton and unlock key

---
+++ Assets/Scripts/UnhideComputerKey.cs
@@ -0,0 +1,41 @@
+﻿//Unlock Key 1 (Computer Key) when correct password is entered. 
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
+
+public class UnhideComputerKey : MonoBehaviour
+{
+    public GameObject userInput;
+    public GameObject verificationMessage;
+    private string enteredPW;
+    public string wrongPasswordMessage;
+    public string correctPasswordMessage;
+
+    bool unhideOnce = false;
+    
+    public void Unhide(GameObject obj)
+    {
+        enteredPW = userInput.GetComponent<Text>().text;
+        //Debug.Log(enteredPW);
+        if (unhideOnce == false)
+        {
+            if (enteredPW == "5418")
+            {
+                obj.SetActive(true);
+                unhideOnce = true;
+                verificationMessage.GetComponent<Text>().text = correctPasswordMessage;
+            }
+             else
+             {
+                verificationMessage.GetComponent<Text>().text = wrongPasswordMessage;
+             }
+        }
+       
+        
+    }
+    
+}
