c4cc27cdf8ea43634706a94ada574388381eb7ca
Carl S <sinocruc@oregonstate.edu>
2020-06-02 20:08:44 -0700

    title screen(start/options menu) complete

---
+++ Assets/TitleScreen/MenuScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Bedroom");
+    }
+    public void OptionsMenu()
+    {
+
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
